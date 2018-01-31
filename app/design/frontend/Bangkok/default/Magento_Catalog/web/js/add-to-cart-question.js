1cce83ef12570164cd551e2520bb891b6588e0c8
Vasyl Sokolyk <vsokolyk@gmail.com>
2018-01-31 16:41:12 +0200

    Fixed redioboxes

--- app/design/frontend/Bangkok/default/Magento_Catalog/web/js/add-to-cart-question.js
+++ app/design/frontend/Bangkok/default/Magento_Catalog/web/js/add-to-cart-question.js
@@ -6,23 +6,52 @@ define([
 
     return function (config, element) {
         element = $(element);
-        var addToCartBtn = $('#product-addtocart-button');
+        var addToCartBtn = $('#product-addtocart-button'),
+            modalWindwow = $('.js-message');
         addToCartBtn.attr('disabled', true);
+
+        function isShowQuestion() {
+            if (Boolean(parseInt($('select.swatch-select.size option:selected').val()))
+                && Boolean(parseInt($('#qty').val()))) {
+                modalWindwow.show();
+            }
+        }
+
+        $(document).on('change', 'select.swatch-select.size', function () {
+            isShowQuestion();
+        });
+
+
+        $(document).on('click', '.swatch-attribute.color .swatch-option.color', function (e) {
+            isShowQuestion();
+        });
+
         $('.js-answer').on('click', function () {
             if ($(this).hasClass('js-show-model')) {
                 var options = {
                     type: 'popup',
                     responsive: true,
                     innerScroll: true,
-                    title: $.mage.__('popup modal title'),
-                    buttons: [{
-                        text: $.mage.__('Continue'),
-                        class: 'product-question-modal-btn',
-                        click: function () {
-                            addToCartBtn.attr('disabled', false);
-                            this.closeModal();
+                    title: $.mage.__('Our consultant can help you with your size.'),
+                    buttons: [
+                        {
+                            text: $.mage.__('Chat with consultant'),
+                            class: 'product-question-modal-btn-consultant',
+                            click: function () {
+                                //TODO: open chant
+                                //addToCartBtn.attr('disabled', false);
+                                this.closeModal();
+                            }
+                        },
+                        {
+                            text: $.mage.__('Add to bag now'),
+                            class: 'product-question-modal-btn-add-to-cart',
+                            click: function () {
+                                $('#product_addtocart_form').submit();
+                                this.closeModal();
+                            }
                         }
-                    }]
+                    ]
                 };
                 var popup = modal(options, element);
                 element.modal('openModal');
