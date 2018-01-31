dcdbeb7bfbed45fc99cfcc11b0f2ba4b12292a96
Vasyl Sokolyk <vsokolyk@gmail.com>
2018-01-31 20:46:54 +0200

    i fixed left desctop menu when sticy header

--- app/design/frontend/Bangkok/default/web/js/global.js
+++ app/design/frontend/Bangkok/default/web/js/global.js
@@ -12,18 +12,23 @@ define([
     //stick header
     //---------------------------------------------
     var previousScroll = 0,
-        pageHeader = $('header.page-header');
+        pageHeader = $('header.page-header'),
+        sidebar =  $('.sidebar.sidebar-additional'),
+        baseSidebarMargin = parseInt(sidebar.css('margin-top'));
     $(window).scroll(function () {
         var currentScroll = $(this).scrollTop();
         if (currentScroll < previousScroll) {
             if (currentScroll > pageHeader.outerHeight()) {
                 pageHeader.addClass('sticky-header');
+                sidebar.css('margin-top', baseSidebarMargin + pageHeader.outerHeight());
 
             } else {
                 pageHeader.removeClass('sticky-header');
+                sidebar.css('margin-top', baseSidebarMargin);
             }
         } else {
             pageHeader.removeClass('sticky-header');
+            sidebar.css('margin-top', baseSidebarMargin);
         }
         previousScroll = currentScroll;
     })
