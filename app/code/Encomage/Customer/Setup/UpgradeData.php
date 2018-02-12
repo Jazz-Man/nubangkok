7286b620b4d57c67d703bc9965347d4729d2f38b
Vasyl Sokolyk <vsokolyk@gmail.com>
2018-02-12 12:15:37 +0200

    task_258 comment:Fixed setup upgrade version

--- app/code/Encomage/Customer/Setup/UpgradeData.php
+++ app/code/Encomage/Customer/Setup/UpgradeData.php
@@ -41,7 +41,7 @@ class UpgradeData implements UpgradeDataInterface
 
     public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
     {
-        if (version_compare($context->getVersion(), '0.0.3', '<')) {
+        if (version_compare($context->getVersion(), '0.0.2', '<')) {
             $setup->startSetup();
             /** @var CustomerSetup $customerSetup */
             $customerSetup = $this->customerSetupFactory->create(['setup' => $setup]);
