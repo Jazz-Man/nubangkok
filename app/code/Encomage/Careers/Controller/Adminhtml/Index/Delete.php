157dd0e027269f6c9ddf8b2257ac465b0d7088d8
Vadim Sapa <89vadimsapa@gmail.com>
2018-02-13 12:16:33 +0200

    #272 Comment: Code refactoring for controllers

--- app/code/Encomage/Careers/Controller/Adminhtml/Index/Delete.php
+++ app/code/Encomage/Careers/Controller/Adminhtml/Index/Delete.php
@@ -1,7 +1,9 @@
 <?php
 namespace Encomage\Careers\Controller\Adminhtml\Index;
-use Encomage\Careers\Model\CareersFactory;
-use Magento\Backend\App\Action;
+
+use \Encomage\Careers\Model\CareersFactory;
+use \Encomage\Careers\Model\ResourceModel\Careers as CareersResource;
+use \Magento\Backend\App\Action;
 
 class Delete extends Action
 {
@@ -9,14 +11,24 @@ class Delete extends Action
      * @var CareersFactory
      */
     private $careersFactory;
+    /**
+     * @var CareersResource
+     */
+    private $careersResource;
 
     /**
      * Delete constructor.
      * @param Action\Context $context
      * @param CareersFactory $careers
+     * @param CareersResource $careersResource
      */
-    public function __construct(Action\Context $context, CareersFactory $careers)
+    public function __construct(
+        Action\Context $context,
+        CareersFactory $careers,
+        CareersResource $careersResource
+    )
     {
+        $this->careersResource = $careersResource;
         $this->careersFactory = $careers;
         parent::__construct($context);
     }
@@ -27,17 +39,19 @@ class Delete extends Action
     public function execute()
     {
         $id = $this->getRequest()->getParam('id');
-
-        if (!($contact = $this->careersFactory->create()->load($id))) {
-            $this->messageManager->addError(__('Unable to proceed. Please, try again.'));
+        $careersModel = $this->careersFactory->create();
+        $vacancy = $this->careersResource->load($careersModel, $id);
+        if (!($vacancy)) {
+            $this->messageManager->addErrorMessage(__('Unable to proceed. Please, try again.'));
             $resultRedirect = $this->resultRedirectFactory->create();
             return $resultRedirect->setPath('*/*/index', array('_current' => true));
         }
-        try{
-            $contact->delete();
-            $this->messageManager->addSuccess(__('Your contact has been deleted !'));
+        
+        try {
+            $this->careersResource->delete($vacancy);
+            $this->messageManager->addSuccessMessage(__('Your contact has been deleted !'));
         } catch (Exception $e) {
-            $this->messageManager->addError(__('Error while trying to delete contact: '));
+            $this->messageManager->addErrorMessage(__('Error while trying to delete contact: '));
             $resultRedirect = $this->resultRedirectFactory->create();
             return $resultRedirect->setPath('*/*/index', array('_current' => true));
         }
