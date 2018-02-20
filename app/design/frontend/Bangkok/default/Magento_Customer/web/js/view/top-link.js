c0c8d491ff302c55a3fef6c3cb7f89ddd0095f49
Vasyl Sokolyk <vsokolyk@gmail.com>
2018-02-20 14:59:15 +0200

    #255 coment: js modify

--- app/design/frontend/Bangkok/default/Magento_Customer/web/js/view/top-link.js
+++ app/design/frontend/Bangkok/default/Magento_Customer/web/js/view/top-link.js
@@ -12,7 +12,7 @@ define([
          * @override
          */
         initialize: function () {
-            var customer = customerData.get('customer');
+            var customer = customerData.get('customer'), self = this;
             $('.customer-links').hide();
             if (customer()['website_id'] !== window.checkout.websiteId) {
                 customerData.reload(['customer'], false);
@@ -20,26 +20,10 @@ define([
             mediaCheck({
                 media: '(max-width: 768px)',
                 entry: $.proxy(function () {
-
-                    $(document).on('click', 'a.top-link-myaccount', function () {
-
-                        // $('html').toggleClass('mobile-account-top-links');
-                        $('html').addClass('mobile-account-top-links');
-
-                        event.preventDefault();
-                    });
-                    $(document).on('click', '.close-account-links-js', function () {
-
-                        // $('html').toggleClass('mobile-account-top-links');
-                        $('html').removeClass('mobile-account-top-links');
-
-                        event.preventDefault();
-                    })
-
+                    self.targetMobile()
                 }, this),
                 exit: $.proxy(function () {
-                    alert('desktop')
-
+                    self.targetDesktop()
                 }, this)
             });
 
@@ -50,6 +34,10 @@ define([
                 links = customerData.get('customer')().customerTopLinks;
             if (links) {
                 $('.customer-links').show();
+
+                for (var i = 0; i < links.length; i++) {
+                    links[i].cssClass = ($('body').hasClass(links[i].handle)) ? 'active' : '';
+                }
             } else {
                 $('.customer-links').hide();
             }
@@ -58,9 +46,22 @@ define([
                     .removeClass('top-link-myaccount')
                     .text($.mage.__('Hi') + ' ' + customer.firstname);
             }
-            return customer.customerTopLinks;
-        }
+            return links;
+        },
+        targetDesktop: function () {
+            //todo: desktop
+        },
+        targetMobile: function () {
 
+            $(document).on('click', 'a.js-customer-top-link', function () {
+                $('html').addClass('mobile-account-top-links');
+                event.preventDefault();
+            });
+            $(document).on('click', '.close-account-links-js', function () {
+                $('html').removeClass('mobile-account-top-links');
+                event.preventDefault();
+            })
+        }
     });
 
 });
