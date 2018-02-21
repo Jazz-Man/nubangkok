6f4ef1da87bf652c1486d188320775c34af94faa
Vasyl Sokolyk <vsokolyk@gmail.com>
2018-02-21 21:12:06 +0200

    #301 comment: add new icons, progress 90%

--- app/code/Encomage/Cms/Setup/UpgradeData.php
+++ app/code/Encomage/Cms/Setup/UpgradeData.php
@@ -72,6 +72,10 @@ class UpgradeData implements UpgradeDataInterface
         if (version_compare($context->getVersion(), '0.0.9', '<')) {
             $this->modify404pageRemoveUnusedPages();
         }
+
+        if (version_compare($context->getVersion(), '0.1.0', '<')) {
+            $this->updateLeftLinksSidebarBlock010();
+        }
     }
 
     private function createBlockForRightImageOnRegisterForm()
@@ -133,6 +137,33 @@ class UpgradeData implements UpgradeDataInterface
 <a class="social-acc-pic" style="padding-top: 0;" href="https://www.facebook.com" target="_blank"> <img src="{{view url=images/facebook.png}}" alt="facebook-icon" /> </a> 
 <a class="social-acc-pic" style="padding-top: 0;" href="https://www.instagram.com" target="_blank"> <img src="{{view url=images/instagram.png}}" alt="instagram-icon" /> </a> 
 <a class="social-acc-pic" style="padding-top: 0;" href="https://www.youtube.com" target="_blank"> <img src="{{view url=images/youtube.png}}" alt="youtube-icon" /> </a> 
+<a class="social-acc-pic" style="padding-top: 0;"> <img src="{{view url=images/empty-link.png}}" alt="youtube-icon" /></a>
+</div>
+<div>
+<p>#nuBangkok</p>
+</div>
+<div><a href='{{store url="policies"}}'>POLICIES</a></div>
+EOD;
+
+        $block = $this->blockRepository->getById('left_sidebar_cms_static_block');
+        $block->setContent($content);
+        $this->blockRepository->save($block);
+    }
+
+    private function updateLeftLinksSidebarBlock010()
+    {
+        $content = <<<EOD
+        <div style="margin-bottom: 1rem;"><a class="violet-link" style="margin-bottom: 1rem;" href='{{store url="why-nu"}}'>WHY <strong style="font-size: 18px;">nu</strong> ?</a></div>
+<div style="margin-bottom: 1rem;"><a href="#">INSPIRATION</a></div>
+<div style="margin-bottom: 1rem;"><a href="#">nu STORIES (yours and ours)</a></div>
+<div style="margin-bottom: 1rem;"><a href='{{store url="customer-care"}}'>CUSTOMER CARE</a></div>
+<div style="margin-bottom: 1rem;"><a href='{{store url="product-care"}}'>PRODUCT CARE</a></div>
+<div style="margin-bottom: 1rem;"><a href='{{store url="contact-us"}}'>CONTACT US</a></div>
+<div><a href="#">CAREER</a></div>
+<div class="social-links" style="margin-bottom: 1rem;">
+<a class="social-acc-pic" style="padding-top: 0;" href="https://www.facebook.com" target="_blank"> <img src="{{view url=images/icons/homePage/Facebook.svg}}" alt="facebook-icon" /> </a> 
+<a class="social-acc-pic" style="padding-top: 0;" href="https://www.instagram.com" target="_blank"> <img src="{{view url=images/icons/homePage/Instagram.svg}}" alt="instagram-icon" /> </a> 
+<a class="social-acc-pic" style="padding-top: 0;" href="https://www.youtube.com" target="_blank"> <img src="{{view url=images/icons/homePage/youtube.svg}}" alt="youtube-icon" /> </a> 
 <a class="social-acc-pic" style="padding-top: 0;"> <img src="{{view url=images/empty-link.png}}" alt="youtube-icon" /></a>
 </div>
 <div>
