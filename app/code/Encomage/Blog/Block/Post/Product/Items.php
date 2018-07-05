cd37ccb341a8ee0cf14fa7e8a5031cf4cd4d7420
Vasyl Sokolyk <vsokolyk@gmail.com>
2018-07-05 13:06:39 +0300

    #511 comment:Added simple product in related product for blog

--- app/code/Encomage/Blog/Block/Post/Product/Items.php
+++ app/code/Encomage/Blog/Block/Post/Product/Items.php
@@ -14,4 +14,25 @@ class Items extends \Magefan\Blog\Block\Post\View\RelatedProducts
         $_collection = $this->_prepareCollection()->getItems();
         return $_collection;
     }
+
+    protected function _prepareCollection()
+    {
+        $post = $this->getPost();
+        $this->_itemCollection = $post->getRelatedProducts()
+            ->addAttributeToSelect('required_options');
+        if ($this->_moduleManager->isEnabled('Magento_Checkout')) {
+            $this->_addProductAttributesAndPrices($this->_itemCollection);
+        }
+        $this->_itemCollection->setPageSize(
+            (int) $this->_scopeConfig->getValue(
+                'mfblog/post_view/related_products/number_of_products',
+                \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+            )
+        );
+        $this->_itemCollection->getSelect()->order('rl.position', 'ASC');
+        foreach ($this->_itemCollection as $product) {
+            $product->setDoNotUseCategoryId(true);
+        }
+        return $this;
+    }
 }
