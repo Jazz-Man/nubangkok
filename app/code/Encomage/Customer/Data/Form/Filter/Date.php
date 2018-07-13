5d163841f3428aca5f1b40a7c961b3e0e06c3689
vadim-sapa <89vadimsapa@gmail.com>
2018-07-13 14:38:27 +0300

    #514 Comment: Fixed bugs

--- app/code/Encomage/Customer/Data/Form/Filter/Date.php
+++ app/code/Encomage/Customer/Data/Form/Filter/Date.php
@@ -14,8 +14,21 @@ class Date extends \Magento\Framework\Data\Form\Filter\Date
         $filterInput = new \Zend_Filter_LocalizedToNormalized(
             ['date_format' => DateTime::DATE_INTERNAL_FORMAT, 'locale' => $this->localeResolver->getLocale()]
         );
+        $filterInternal = new \Zend_Filter_NormalizedToLocalized(
+            ['date_format' => $this->_dateFormat, 'locale' => $this->localeResolver->getLocale()]
+        );
 
         $value = $filterInput->filter($value);
+        $value = $filterInternal->filter($value);
+        if (is_string($value)) {
+            $result = explode(',', $value);
+            if (count($result) > 1) {
+                $value = '';
+                foreach ($result as $date) {
+                    $value .= $date;
+                }
+            }
+        }
         return $value;
     }
 }
