28af1c5096e87d2118e8630a1789906597280660
vadim-sapa <89vadimsapa@gmail.com>
2018-09-10 20:21:16 +0300

    547 Comment: Fixed the style

--- app/design/frontend/Bangkok/default/Encomage_Nupoints/web/js/nupoints/selector-redeem.js
+++ app/design/frontend/Bangkok/default/Encomage_Nupoints/web/js/nupoints/selector-redeem.js
@@ -34,23 +34,23 @@ define([
             initUi(customerNupoints);
 
         }
-
-        redeemButton.on('click', function () {
-            var redeemNupoints = select.find('.js-selected-option-container').data('selectValue');
-            $.ajax({
-                url: urlBuilder.build(config.nupointsRedeemUrl),
-                data: {'redeem_nupoints': redeemNupoints},
-                type: 'post',
-                dataType: 'json',
-                async: false,
-                showLoader: true,
-                success: function (response) {
-                    if (response.customer_nupoints && response.success) {
-                        $element.find('.js-customer-nupoints-balance').html(response.customer_nupoints);
-                    }
-                }
-            });
-            $element.find('.js-customer-nuponts-redeem').attr('disabled', true);
-        });
+        
+        // redeemButton.on('click', function () {
+        //     var redeemNupoints = select.find('.js-selected-option-container').data('selectValue');
+        //     $.ajax({
+        //         url: urlBuilder.build(config.nupointsRedeemUrl),
+        //         data: {'redeem_nupoints': redeemNupoints},
+        //         type: 'post',
+        //         dataType: 'json',
+        //         async: false,
+        //         showLoader: true,
+        //         success: function (response) {
+        //             if (response.customer_nupoints && response.success) {
+        //                 $element.find('.js-customer-nupoints-balance').html(response.customer_nupoints);
+        //             }
+        //         }
+        //     });
+        //     $element.find('.js-customer-nuponts-redeem').attr('disabled', true);
+        // });
     }
 });
