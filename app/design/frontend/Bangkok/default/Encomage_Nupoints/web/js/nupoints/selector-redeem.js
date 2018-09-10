35940de0b03ea3d06c1ebefba748ab63fce54c74
Vasyl Sokolyk <vsokolyk@gmail.com>
2018-09-10 20:30:30 +0300

    547 Comment: Removed the extra code

--- app/design/frontend/Bangkok/default/Encomage_Nupoints/web/js/nupoints/selector-redeem.js
+++ app/design/frontend/Bangkok/default/Encomage_Nupoints/web/js/nupoints/selector-redeem.js
@@ -1,8 +1,7 @@
 define([
     'jquery',
-    'Magento_Customer/js/customer-data',
-    'mage/url'
-], function ($, customerData, urlBuilder) {
+    'Magento_Customer/js/customer-data'
+], function ($, customerData) {
     'use strict';
     return function (config, element) {
         function initUi(customerDataObject) {
@@ -14,8 +13,6 @@ define([
         }
         var $element = $(element),
             customerNupoints = customerData.get('nupoints')(),
-            select = $element.find('.js-nupoints-rates'),
-            redeemButton = $element.find('.js-customer-nuponts-redeem'),
             expiredSectionNames = customerData.getExpiredSectionNames();
 
         customerData.get('nupoints').subscribe(function (updatedCart) {
@@ -34,23 +31,5 @@ define([
             initUi(customerNupoints);
 
         }
-        
-        // redeemButton.on('click', function () {
-        //     var redeemNupoints = select.find('.js-selected-option-container').data('selectValue');
-        //     $.ajax({
-        //         url: urlBuilder.build(config.nupointsRedeemUrl),
-        //         data: {'redeem_nupoints': redeemNupoints},
-        //         type: 'post',
-        //         dataType: 'json',
-        //         async: false,
-        //         showLoader: true,
-        //         success: function (response) {
-        //             if (response.customer_nupoints && response.success) {
-        //                 $element.find('.js-customer-nupoints-balance').html(response.customer_nupoints);
-        //             }
-        //         }
-        //     });
-        //     $element.find('.js-customer-nuponts-redeem').attr('disabled', true);
-        // });
     }
 });
