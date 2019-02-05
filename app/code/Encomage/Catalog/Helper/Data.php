1c0febc02f7ed75bccc2993835a91aecab1d1fdd
Artur Gevorkyan <jakord2@gmail.com>
2019-02-05 12:52:27 +0200

    670 comment:fixed the correct display product on plp

--- app/code/Encomage/Catalog/Helper/Data.php
+++ app/code/Encomage/Catalog/Helper/Data.php
@@ -2,15 +2,71 @@
 
 namespace Encomage\Catalog\Helper;
 
-class Data extends \Magento\Framework\App\Helper\AbstractHelper
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Catalog\Model\Category;
+use Magento\Catalog\Model\Product;
+use Magento\Framework\App\Helper\Context;
+use Magento\CatalogInventory\Api\Data\StockItemInterfaceFactory;
+
+/**
+ * Class Data
+ * @package Encomage\Catalog\Helper
+ */
+class Data extends AbstractHelper
 {
-    public function getNotifyMeEmptyCategoryUrl(\Magento\Catalog\Model\Category $category)
+    /**
+     * @var StockItemInterfaceFactory
+     */
+    protected $stockItemInterfaceFactory;
+
+    /**
+     * Data constructor.
+     * @param Context $context
+     * @param StockItemInterfaceFactory $stockItemInterfaceFactory
+     */
+    public function __construct(
+        Context $context,
+        StockItemInterfaceFactory $stockItemInterfaceFactory
+    )
+    {
+        parent::__construct($context);
+        $this->stockItemInterfaceFactory = $stockItemInterfaceFactory;
+    }
+
+    /**
+     * @param Category $category
+     * @return string
+     */
+    public function getNotifyMeEmptyCategoryUrl(Category $category)
     {
         return $this->_getUrl('bangkok_catalog/category/notifyEmpty', ['category_id' => (int)$category->getId()]);
     }
 
-    public function getFacebookSharedLink(\Magento\Catalog\Model\Product $product)
+    /**
+     * @param Product $product
+     * @return string
+     */
+    public function getFacebookSharedLink(Product $product)
     {
         return 'http://www.facebook.com/sharer/sharer.php?u=' . $product->getProductUrl();
+    }
+
+    /**
+     * @param int $productId
+     * @return bool
+     */
+    public function getProductStockStatus(int $productId)
+    {
+        $stockData = $this->stockItemInterfaceFactory->create();
+        $stockData->load($productId);
+        if (!$stockData->getId()){
+            return false;
+        }
+
+        if ($stockData->getQty() && $stockData->getIsInStock()) {
+            return true;
+        }
+
+        return false;
     }
 }
