44cd8e1a640ae19a700d4fe968223e7d62c9ca55
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-06-28 20:38:17 +0300

    Swatches fix

---
+++ app/code/NuBangkok/All/Override/Swatches/Block/Adminhtml/Attribute/Edit/Options/Visual.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace NuBangkok\All\Override\Swatches\Block\Adminhtml\Attribute\Edit\Options;
+
+class Visual extends \Magento\Swatches\Block\Adminhtml\Attribute\Edit\Options\Visual
+{
+
+    protected $_template = 'NuBangkok_All::catalog/product/attribute/visual.phtml';
+
+}
