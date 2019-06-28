1c69c2adebcfc982c72d36d5c9ee044f3360e52a
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-06-28 21:01:31 +0300

    Swatches fix

--- app/code/NuBangkok/All/Override/Swatches/Block/Adminhtml/Attribute/Edit/Options/Visual.php
+++ app/code/NuBangkok/All/Override/Swatches/Block/Adminhtml/Attribute/Edit/Options/Visual.php
@@ -5,6 +5,21 @@ namespace NuBangkok\All\Override\Swatches\Block\Adminhtml\Attribute\Edit\Options
 class Visual extends \Magento\Swatches\Block\Adminhtml\Attribute\Edit\Options\Visual
 {
 
-    protected $_template = 'NuBangkok_All::catalog/product/attribute/visual.phtml';
+    protected function reformatSwatchLabels($swatchStoreValue = null)
+    {
+        if ($swatchStoreValue === null) {
+            return;
+        }
+        $newSwatch = [];
+        foreach ($swatchStoreValue as $key => $value) {
+            if ($value[0] == '#') {
+                $newSwatch[$key] = 'background: '.$value;
+            } elseif ($value[0] == '/') {
+                $mediaUrl = $this->swatchHelper->getSwatchMediaUrl();
+                $newSwatch[$key] = 'background: url('.$mediaUrl.$value.'); background-size: cover;';
+            }
+        }
+        return $newSwatch;
+    }
 
 }
