7eeb03ad1f669f1788ffaa27a10e70b7e9818632
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-07-21 01:32:55 +0300

    updated code style

--- app/code/Encomage/Blog/Block/Post/View/Share.php
+++ app/code/Encomage/Blog/Block/Post/View/Share.php
@@ -2,9 +2,21 @@
 
 namespace Encomage\Blog\Block\Post\View;
 
-use Magefan\Blog\Block\Post\AbstractPost;
 use Encomage\Theme\Block\Html\Page\FacebookShareLinkInterface;
+use Encomage\Theme\Helper\Data;
+use Magefan\Blog\Block\Post\AbstractPost;
+use Magefan\Blog\Model\Post;
+use Magefan\Blog\Model\PostFactory;
+use Magefan\Blog\Model\Url;
+use Magento\Cms\Model\Template\FilterProvider;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Element\Template\Context;
 
+/**
+ * Class Share
+ *
+ * @package Encomage\Blog\Block\Post\View
+ */
 class Share extends AbstractPost implements FacebookShareLinkInterface
 {
 
@@ -15,26 +27,26 @@ class Share extends AbstractPost implements FacebookShareLinkInterface
 
     /**
      * Share constructor.
+     *
      * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \Magefan\Blog\Model\Post $post
-     * @param \Magento\Framework\Registry $coreRegistry
-     * @param \Magento\Cms\Model\Template\FilterProvider $filterProvider
-     * @param \Magefan\Blog\Model\PostFactory $postFactory
-     * @param \Magefan\Blog\Model\Url $url
-     * @param \Encomage\Theme\Helper\Data $helper
-     * @param array $data
+     * @param \Magefan\Blog\Model\Post                         $post
+     * @param \Magento\Framework\Registry                      $coreRegistry
+     * @param \Magento\Cms\Model\Template\FilterProvider       $filterProvider
+     * @param \Magefan\Blog\Model\PostFactory                  $postFactory
+     * @param \Magefan\Blog\Model\Url                          $url
+     * @param \Encomage\Theme\Helper\Data                      $helper
+     * @param array                                            $data
      */
     public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \Magefan\Blog\Model\Post $post,
-        \Magento\Framework\Registry $coreRegistry,
-        \Magento\Cms\Model\Template\FilterProvider $filterProvider,
-        \Magefan\Blog\Model\PostFactory $postFactory,
-        \Magefan\Blog\Model\Url $url,
-        \Encomage\Theme\Helper\Data $helper,
+        Context $context,
+        Post $post,
+        Registry $coreRegistry,
+        FilterProvider $filterProvider,
+        PostFactory $postFactory,
+        Url $url,
+        Data $helper,
         array $data = []
-    )
-    {
+    ) {
         parent::__construct($context, $post, $coreRegistry, $filterProvider, $postFactory, $url, $data);
         $this->helper = $helper;
     }
@@ -57,11 +69,13 @@ class Share extends AbstractPost implements FacebookShareLinkInterface
 
     /**
      * @param string $cssClass
+     *
      * @return $this|mixed
      */
     public function setCssClass(string $cssClass)
     {
         $this->setData('css_class', $cssClass);
+
         return $this;
     }
 }
