6c9335034e1a8cd7a4ab0dfd6e557694c222cdd5
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-07-21 00:00:02 +0300

    cleanup code

--- app/code/ErpAPI/ErpAPICommand/Helper/ApiClient.php
+++ app/code/ErpAPI/ErpAPICommand/Helper/ApiClient.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace ErpAPI\ErpAPICommand\Helper;
-
 
 use GuzzleHttp\Client;
 use function GuzzleHttp\json_decode as json_decodeAlias;
@@ -10,24 +8,21 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 use Psr\Http\Message\ResponseInterface;
 
 /**
- * Class ApiClient
- *
- * @package ErpAPI\ErpAPICommand\Helper
+ * Class ApiClient.
  */
 class ApiClient
 {
+    public const COMPCODE = 'erp_etoday_settings/erp_authorization/compcode';
 
-    const COMPCODE = 'erp_etoday_settings/erp_authorization/compcode';
+    public const HOST_NAME = 'erp_etoday_settings/erp_authorization/host_name';
 
-    const HOST_NAME = 'erp_etoday_settings/erp_authorization/host_name';
+    public const LOGIN = 'erp_etoday_settings/erp_authorization/login';
 
-    const LOGIN = 'erp_etoday_settings/erp_authorization/login';
+    public const PASSWORD = 'erp_etoday_settings/erp_authorization/password';
 
-    const PASSWORD = 'erp_etoday_settings/erp_authorization/password';
+    public const TEST_MODE = 'erp_etoday_settings/erp_authorization/enabled_test_mode';
 
-    const TEST_MODE = 'erp_etoday_settings/erp_authorization/enabled_test_mode';
-
-    const WAREHOUSE_CODE = 'erp_etoday_settings/erp_authorization/warehouse_code';
+    public const WAREHOUSE_CODE = 'erp_etoday_settings/erp_authorization/warehouse_code';
 
     /**
      * @var \GuzzleHttp\Client
@@ -48,32 +43,29 @@ class ApiClient
      *
      * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
      */
-    public function __construct(ScopeConfigInterface $scopeConfig) {
-
-
-        $this->_host_name         = $scopeConfig->getValue(self::HOST_NAME);
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->_host_name = $scopeConfig->getValue(self::HOST_NAME);
 
         $this->client = new Client([
             'timeout' => 30,
             'headers' => [
-                'Accept'       => 'application/json',
+                'Accept' => 'application/json',
                 'Content-Type' => 'application/json',
             ],
         ]);
 
         $this->defaults = [
-            'userAccount'   => $scopeConfig->getValue(self::LOGIN),
-            'userPassword'  => $scopeConfig->getValue(self::PASSWORD),
-            'compCode'      => $scopeConfig->getValue(self::COMPCODE),
+            'userAccount' => $scopeConfig->getValue(self::LOGIN),
+            'userPassword' => $scopeConfig->getValue(self::PASSWORD),
+            'compCode' => $scopeConfig->getValue(self::COMPCODE),
             'warehouseCode' => $scopeConfig->getValue(self::WAREHOUSE_CODE),
         ];
 
-        if ((bool)$scopeConfig->getValue(self::TEST_MODE)) {
+        if ((bool) $scopeConfig->getValue(self::TEST_MODE)) {
             $this->defaults['testmode'] = 1;
         }
-
     }
-
 
     /**
      * @param string $point
@@ -83,7 +75,6 @@ class ApiClient
      */
     public function getData(string $point, array $query = [])
     {
-
         $query = array_merge($this->defaults, $query);
 
         return $this->client->get("{$this->_host_name}/{$point}", [
@@ -96,16 +87,16 @@ class ApiClient
      *
      * @return bool|mixed
      */
-    public function parseBody(ResponseInterface $response){
-        if (200 === $response->getStatusCode()){
+    public function parseBody(ResponseInterface $response)
+    {
+        if (200 === $response->getStatusCode()) {
             $body = $response->getBody()->getContents();
 
             $response_data = json_decodeAlias($body);
 
-            return !empty($response_data)? $response_data: false;
+            return !empty($response_data) ? $response_data : false;
         }
 
         return false;
     }
-
 }
