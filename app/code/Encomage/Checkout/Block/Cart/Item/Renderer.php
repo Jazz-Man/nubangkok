799bb834a87d9d303f52b711e7f280d54d5a263a
Vasyl Sokolik <vsokolyk@gmail.com>
2019-08-08 04:08:08 +0300

    fix code style

--- app/code/Encomage/Checkout/Block/Cart/Item/Renderer.php
+++ app/code/Encomage/Checkout/Block/Cart/Item/Renderer.php
@@ -2,72 +2,48 @@
 
 namespace Encomage\Checkout\Block\Cart\Item;
 
-use Magento\Framework\Pricing\PriceCurrencyInterface;
-use Magento\Framework\View\Element\Message\InterpretationStrategyInterface;
-use Magento\Swatches\Model\ResourceModel\Swatch\CollectionFactory as SwatchCollectionFactory;
+use Magento\ConfigurableProduct\Block\Cart\Item\Renderer\Configurable;
+use Magento\Framework\App\ObjectManager;
+use Magento\Swatches\Model\ResourceModel\Swatch\Collection;
 
-class Renderer extends \Magento\ConfigurableProduct\Block\Cart\Item\Renderer\Configurable
+/**
+ * Class Renderer.
+ */
+class Renderer extends Configurable
 {
-    /**
-     * @var SwatchCollectionFactory
-     */
-    private $swatchCollectionFactory;
-
-    public function __construct(
-        \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Catalog\Helper\Product\Configuration $productConfig,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        \Magento\Catalog\Block\Product\ImageBuilder $imageBuilder,
-        \Magento\Framework\Url\Helper\Data $urlHelper,
-        \Magento\Framework\Message\ManagerInterface $messageManager,
-        PriceCurrencyInterface $priceCurrency,
-        \Magento\Framework\Module\Manager $moduleManager,
-        InterpretationStrategyInterface $messageInterpretationStrategy,
-        SwatchCollectionFactory $swatchCollectionFactory,
-        array $data
-    )
-    {
-        $this->swatchCollectionFactory = $swatchCollectionFactory;
-        parent::__construct(
-            $context,
-            $productConfig,
-            $checkoutSession,
-            $imageBuilder,
-
-            $urlHelper,
-            $messageManager,
-            $priceCurrency,
-            $moduleManager,
-            $messageInterpretationStrategy,
-            $data
-        );
-    }
-
     /**
      * @return array
      */
-    public function colorOption()
+    public function colorOption(): array
     {
         $options = $this->getOptionList();
+
         $colorInfo = [];
         foreach ($options as $kay => $option) {
             $colorInfo[$kay] = $option;
-            if ($option['label'] == 'Color' && isset($option['option_value'])) {
+            if ('Color' === $option['label'] && isset($option['option_value'])) {
                 $colorInfo[$kay]['rgb_code'] = $this->getColorRgbCode($option['option_value']);
             }
         }
+
         return $colorInfo;
     }
 
     /**
      * @param $colorCode
-     * @return null
+     *
+     * @return string|null
      */
     protected function getColorRgbCode($colorCode)
     {
-        $swatchCollection = $this->swatchCollectionFactory->create();
+        $objectManager = ObjectManager::getInstance();
+
+        $swatchCollection = $objectManager->create(Collection::class);
+
         $swatchCollection->addFilterByOptionsIds([$colorCode]);
+        /** @var \Magento\Swatches\Model\Swatch $item */
         $item = $swatchCollection->getFirstItem();
-        return $item->getValue() ? $item->getValue() : null;
+
+        return $item->offsetGet('value');
     }
 }
