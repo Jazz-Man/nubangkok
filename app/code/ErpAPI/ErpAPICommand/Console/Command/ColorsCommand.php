9755f717a5cbe7c434d85792919c436a4f56612f
Vasyl Sokolik <vsokolyk@gmail.com>
2019-09-08 02:28:33 +0300

    delete all products

--- app/code/ErpAPI/ErpAPICommand/Console/Command/ColorsCommand.php
+++ app/code/ErpAPI/ErpAPICommand/Console/Command/ColorsCommand.php
@@ -2,11 +2,7 @@
 
 namespace ErpAPI\ErpAPICommand\Console\Command;
 
-use Encomage\ErpIntegration\Helper\CacheFile;
-use Encomage\ErpIntegration\Helper\Data;
 use Exception;
-use Magento\Catalog\Api\ProductRepositoryInterface;
-use Magento\Catalog\Model\ResourceModel\Product as ProductResource;
 use Magento\Catalog\Model\ResourceModel\Product\Collection as ProductCollectionAlias;
 use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
 use Magento\Framework\App\State;
@@ -22,62 +18,31 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class ColorsCommand extends Command
 {
-
-
-
     /**
      * @var \Magento\Framework\App\State
      */
     private $state;
 
-
-    /**
-     * @var \Encomage\ErpIntegration\Helper\CacheFile
-     */
-    private $cacheFile;
-    /**
-     * @var \Encomage\ErpIntegration\Helper\Data
-     */
-    private $helper;
-    /**
-     * @var \Magento\Catalog\Model\ResourceModel\Product
-     */
-    private $productResource;
     /**
      * @var \Magento\Framework\ObjectManagerInterface
      */
     private $objectManager;
-    /**
-     * @var \Magento\Catalog\Api\ProductRepositoryInterface
-     */
-    private $productRepository;
 
     /**
      * ColorsCommand constructor.
      *
-     * @param \Magento\Framework\App\State                    $state
-     * @param \Magento\Framework\ObjectManagerInterface       $objectManager
-     *
-     * @param \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
-     * @param \Magento\Catalog\Model\ResourceModel\Product    $productResource
-     * @param \Encomage\ErpIntegration\Helper\Data            $helper
+     * @param \Magento\Framework\App\State              $state
+     * @param \Magento\Framework\ObjectManagerInterface $objectManager
      */
     public function __construct(
         State $state,
-        ObjectManagerInterface $objectManager,
-        ProductRepositoryInterface $productRepository,
-        ProductResource $productResource,
-        Data $helper
+        ObjectManagerInterface $objectManager
     ) {
-        $this->state                     = $state;
+        $this->state = $state;
 
         parent::__construct();
 
-        $this->cacheFile = new CacheFile($objectManager);
-        $this->helper = $helper;
-        $this->productResource = $productResource;
         $this->objectManager = $objectManager;
-        $this->productRepository = $productRepository;
     }
 
     protected function configure()
@@ -87,9 +52,8 @@ class ColorsCommand extends Command
         try {
             $this->state->setAreaCode('adminhtml');
         } catch (LocalizedException $e) {
-//            $this->state->setAreaCode('adminhtml');
+            //            $this->state->setAreaCode('adminhtml');
         }
-
 
         parent::configure();
     }
@@ -107,29 +71,19 @@ class ColorsCommand extends Command
         $output->writeln('Setup colors!');
 
         /** @var ProductCollectionAlias $collection */
-
         $collection = $this->objectManager->create(CollectionFactory::class)->create();
         $collection->addAttributeToSelect('*');
         $collection->getProductTypeIds();
         $collection->load();
 
-
         /** @var \Magento\Catalog\Model\Product $product */
-        foreach ($collection as $product){
-
+        foreach ($collection as $product) {
             try {
-
-                if ($product->getTypeId() === 'simple'){
-                    dump("Delete Product: '{$product->getName()}'");
-                    $product->delete();
-                }
+                dump("Delete Product: '{$product->getName()}'");
+                $product->delete();
             } catch (Exception $e) {
                 dump($e->getMessage());
             }
         }
-
-//        dump($this->registry);
-
     }
-
 }
