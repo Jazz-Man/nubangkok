b899290e807f46f002559a4e27ed08c1d65eee19
Vasyl Sokolyk <vsokolyk@gmail.com>
2019-10-17 01:12:57 +0300

    added module dependencies

--- app/code/Encomage/Customer/Plugin/CustomerData/Customer.php
+++ app/code/Encomage/Customer/Plugin/CustomerData/Customer.php
@@ -6,15 +6,43 @@ use Magento\Framework\Serialize\Serializer\Json;
 use Magento\Framework\UrlInterface;
 use Magento\Customer\Model\Session as CustomerSession;
 
+/**
+ * Class Customer
+ *
+ * @package Encomage\Customer\Plugin\CustomerData
+ */
 class Customer
 {
 
+    /**
+     * @var \Magento\Framework\Serialize\Serializer\Json
+     */
     private $json;
+    /**
+     * @var \Magento\Framework\UrlInterface
+     */
     private $urlBuilder;
+    /**
+     * @var \Magento\Customer\Model\Session
+     */
     private $customerSession;
+    /**
+     * @var array|mixed
+     */
     private $loggedCustomerUrls;
+    /**
+     * @var array|mixed
+     */
     private $notLoggedCustomerUrls;
 
+    /**
+     * Customer constructor.
+     *
+     * @param \Magento\Framework\Serialize\Serializer\Json $json
+     * @param \Magento\Framework\UrlInterface              $urlBuilder
+     * @param \Magento\Customer\Model\Session              $customerSession
+     * @param array                                        $data
+     */
     public function __construct(
         Json $json,
         UrlInterface $urlBuilder,
@@ -25,18 +53,24 @@ class Customer
         $this->json = $json;
         $this->urlBuilder = $urlBuilder;
         $this->customerSession = $customerSession;
-        $this->loggedCustomerUrls = (isset($data['loggedCustomerUrls'])) ? $data['loggedCustomerUrls'] : [];
-        $this->notLoggedCustomerUrls = (isset($data['notLoggedCustomerUrls'])) ? $data['notLoggedCustomerUrls'] : [];
+        $this->loggedCustomerUrls = !empty($data['loggedCustomerUrls']) ? $data['loggedCustomerUrls'] : [];
+        $this->notLoggedCustomerUrls = !empty($data['notLoggedCustomerUrls']) ? $data['notLoggedCustomerUrls'] : [];
     }
 
+    /**
+     * @param \Magento\Customer\CustomerData\Customer $subject
+     * @param                                         $result
+     *
+     * @return mixed
+     */
     public function afterGetSectionData(\Magento\Customer\CustomerData\Customer $subject, $result)
     {
-        $links = ($this->customerSession->isLoggedIn()) ? $this->loggedCustomerUrls : $this->notLoggedCustomerUrls;
+        $links = $this->customerSession->isLoggedIn() ? $this->loggedCustomerUrls : $this->notLoggedCustomerUrls;
         foreach ($links as $link) {
             $result['customerTopLinks'][] = [
                 'label' => __($link['label']),
                 'url' => $this->urlBuilder->getUrl($link['path']),
-                'handle' => (isset($link['handle'])) ? $link['handle'] : null
+                'handle' => !empty($link['handle']) ? $link['handle'] : null
             ];
         }
         return $result;
